f4233fb7122ada2d0bdc1d61ae8817f6f1e2d736
FRANCISCO WALA NORONHA MACEDO <25203678@dsn127m1246728.cfp127aluno.local>
2026-09-04 17:00:48 -0300

    first commit

--- src/model/Pessoa.kt
+++ src/model/Pessoa.kt
@@ -1,5 +1,4 @@
 package model
 
-class pessoa {
-
+class Pessoa {
 }
---
+++ src/model/Produto.kt
@@ -0,0 +1,20 @@
+package model
+
+import kotlin.math.max
+
+class Produtos (var produto : String, var preco : Int) {
+
+    val produtos = listOf(produto, preco)
+
+
+//    val numbers = listOf (12, 22)
+//
+//    val numero = 1
+//
+//    val alguem = Pessoa("Joao", 25, "Barueri")
+//
+//    val pessoas = listOf(Pessoa())
+
+
+
+}
